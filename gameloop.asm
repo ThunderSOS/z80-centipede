2268fffc00ac209d01c745f06f5cc4b641f8d88f
ThunderSOS <ThunderSOS@users.noreply.github.com>
2020-08-10 17:27:26 +0100

    Minor refactor
    Just tidying really

--- gameloop.asm
+++ gameloop.asm
@@ -2,19 +2,19 @@
 init_game               ld a, 6                         ; ld a, (screen_colour)
                         ld (23693),a                    ; set our screen colours.
                         call 3503                       ; clear the screen.
-                        xor a
-                        ld (num_mushrooms_left), a
+                        xor a                           ;
+                        ld (num_mushrooms_left), a      ;
                         call 8859                       ; set the border permanent
-                        ld a, 3
-                        ld (lives), a
+                        ld a, 3                         ;
+                        ld (lives), a                   ;
                         ld iy, player                   ;
                         call init_mushrooms             ;
                         call display_score              ;
-                        call display_lives
+                        call display_lives              ;
                         call init_segments              ;
                         call draw_all_segments          ;
-                        call init_player
-                        ld (iy+pl_flags), 0
+                        call init_player                ;
+                        ld (iy+pl_flags), 0             ;
                         call draw_player                ;
                         ret                             ;
 
@@ -27,8 +27,8 @@ gameloop                ld a, 0                         ;
                         call check_collision            ;
                         call update_bullet              ;
                         call check_bullet_collision     ;
-                        call colour_bullet_square       ;
-                        call check_bseg_collision
+                        call colour_bullet_square       ; bit of a hack this
+                        call check_bseg_collision       ;
                         xor a                           ;
                         out(254), a                     ;
                         ret                             ;
--- menu.asm
+++ menu.asm
@@ -1,9 +1,9 @@
 
-centipede_title         defb "C e n t i p e d e", 0
-option_1                defb "1 - Start", 0
-option_2                defb "0 - Quit", 0
+centipede_title         defb "C e n t i p e d e", 0     ;
+option_1                defb "1 - Start", 0             ;
+option_2                defb "0 - Quit", 0              ;
 
-show_menu               di
+show_menu               di                              ;
                         ld a, 6                         ;
                         ld (23693),a                    ; set our screen colours.
                         call 3503                       ; clear the screen.
@@ -13,28 +13,28 @@ show_menu               di
                         ld l, 7*8                       ;
                         ld de, centipede_title          ;
                         call pr_str_dbl_ht              ;
-                        ld h, 7*8
-                        ld l, 11*8
-                        ld de, option_1
-                        call pr_str_dbl_ht
-                        ld h, 10*8
-                        ld l, 11*8
-                        ld de, option_2
-                        call pr_str_dbl_ht
+                        ld h, 7*8                       ;
+                        ld l, 11*8                      ;
+                        ld de, option_1                 ;
+                        call pr_str_dbl_ht              ;
+                        ld h, 10*8                      ;
+                        ld l, 11*8                      ;
+                        ld de, option_2                 ;
+                        call pr_str_dbl_ht              ;
 
-wait_loop               ld bc, zeuskeyaddr("0")
+wait_loop               ld bc, zeuskeyaddr("0")         ;
                         in a, (c)                       ;
                         ld e, zeuskeymask("0")          ;
                         and e                           ;
                         cp e                            ;
-                        jp nz, quit_game                ;
-                        ld bc, zeuskeyaddr("1")
-                        in a, (c)
-                        ld e, zeuskeymask("1")
-                        and e
-                        cp e
-                        jp nz, start_game
-                        jr wait_loop                     ;
-quit_game               rst 0
-start_game              jp interrupt_setup
+                        jp nz, 0                        ;
+                        ld bc, zeuskeyaddr("1")         ;
+                        in a, (c)                       ;
+                        ld e, zeuskeymask("1")          ;
+                        and e                           ;
+                        cp e                            ;
+                        jp nz, interrupt_setup          ;
+                        jr wait_loop                    ;
+
+
 
